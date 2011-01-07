1b76e486fb4b605b71b912fd852e54d054725974
Adrian Salceanu <adrian@reveloper.com>
2011-01-07 21:11:42 +0200

    work

--- app/helpers/application_helper.rb
+++ app/helpers/application_helper.rb
@@ -1,5 +1,21 @@
 module ApplicationHelper
   
+  def pomodoros_as_image(nr_of_pomodoros, inactive = false)
+    output = ""
+    nr_of_pomodoros.times do |i|
+      output += image_tag("tomato" + (inactive ? "_gray" : "") + ".jpg", :size => "16x16" )
+    end
+    raw output
+  end
   
+  def pomodoros_completed_ratio(completed_pomodoros, estimated_pomodoros)
+    output = pomodoros_as_image(completed_pomodoros)
+    
+    pomodoros_left = estimated_pomodoros - completed_pomodoros;
+    
+    return raw output if pomodoros_left < 0
+    
+    raw output += pomodoros_as_image(pomodoros_left, true)
+  end
   
 end
--- public/javascripts/application.js
+++ public/javascripts/application.js
@@ -1,8 +1,21 @@
 // Place your application-specific JavaScript functions and classes here
 // This file is automatically included by javascript_include_tag :defaults
+
+var played_sounds = new Array();
+played_sounds[23] = false;
+played_sounds[19] = false;
+played_sounds[14] = false;
+played_sounds[9] = false;
+played_sounds[4] = false;
+played_sounds[2] = false;
+played_sounds[0] = false;
+
+var clock_audio;
+
 $(document).ready(function(){
 	// setup pomodoro timer
 	decreaseTimer();
+	//setTimeout("testSounds()", 5000);
 });
 
 function decreaseTimer() {
@@ -10,12 +23,20 @@ function decreaseTimer() {
 	
 	if(currentTime < 0){
 		currentTime = 0;
+		if( $('#pomodoro_submit[value="Complete"]').length == 0 ) window.location.reload();
 	} else {
 		setTimeout("decreaseTimer()", 1000);
 	}
 	
 	minutes = parseInt(currentTime / 60);
 	seconds = currentTime % 60;
+	if(seconds < 10) seconds = '0' + seconds;
+	
+	if(minutes > 0) {
+		announceTimeLeft(minutes);
+	} else if(currentTime == 0) {
+		announceTimeLeft(0);
+	}
 	
 	$(".timer .time").attr("data-seconds", currentTime);
 	$(".timer .time").html(minutes + ":" + seconds);
@@ -27,3 +48,46 @@ $(document).ready(function(){
 		console.log('moo');
 	})
 });
+
+
+function testSounds() {
+	for(var i in played_sounds) {
+		if(!played_sounds[i]) {
+			console.log("starting " + i);
+			
+			announceTimeLeft(i);
+			played_sounds[i] = true;
+			setTimeout("testSounds()", 5000);
+			break;
+		}
+	}
+}
+
+function announceTimeLeft(minutes) {
+	
+	if(played_sounds[minutes] !== undefined && !played_sounds[minutes]) {
+		
+		sound_file = "sounds/text-to-speech/" + (parseInt(minutes) +1) + ".ogg";
+		if(minutes == 0 && $('#pomodoro_submit[value="Complete"]').length == 0) {
+			sound_file = "sounds/text-to-speech/0.ogg";
+		} 
+		
+		console.log(sound_file);
+		
+		audio = new Audio(sound_file);
+		audio.play();
+	  	
+		played_sounds[minutes] = true;
+	}
+}
+
+function playClockSound() {
+	sound_file = "/sounds/clocks/egg_timer.ogg"
+	
+	clock_audio = new Audio(sound_file);
+	clock_audio.play();
+}
+
+function stopClockSound() {
+	clock_audio.stop();
+}
