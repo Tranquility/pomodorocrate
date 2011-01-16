0874d757870117687cf8657c1a6fa4012cf35a2e
Adrian Salceanu <adrian@reveloper.com>
2011-01-16 19:10:52 +0200

    preparing for deployment on heroku

--- app/controllers/application_controller.rb
+++ app/controllers/application_controller.rb
@@ -6,7 +6,13 @@ class ApplicationController < ActionController::Base
   def setup_widgets
     @pomodoro = Pomodoro.where( :completed => nil ).first
     @break = Break.where( :completed => nil ).first
-    @recent_pomodoros = Pomodoro.successful_and_completed.group("activity_id").order("pomodoros.created_at DESC").limit(5)
+    
+    if Rails.env.production?
+      @recent_pomodoros = Pomodoro.successful_and_completed.select("activity_id, created_at, *").group("activity_id").order("pomodoros.created_at DESC").limit(5)
+    else
+      @recent_pomodoros = Pomodoro.successful_and_completed.group("activity_id").order("pomodoros.created_at DESC").limit(5)
+    end
+    
     @upcoming_activities = Activity.where(:deadline => Time.now.midnight..(Time.now.midnight + 7.days), :completed => false)
     
     @today_remaining_pomodoros = 0
