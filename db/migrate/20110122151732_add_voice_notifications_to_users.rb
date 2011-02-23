1e83fa76d899cd05e58426962e548fe22dad8b82
Adrian Salceanu <adrian@reveloper.com>
2011-02-23 22:44:27 +0200

    deployment

--- db/migrate/20110122151732_add_voice_notifications_to_users.rb
+++ db/migrate/20110122151732_add_voice_notifications_to_users.rb
@@ -1,6 +1,6 @@
 class AddVoiceNotificationsToUsers < ActiveRecord::Migration
   def self.up
-    add_column :users, :voice_notifications, :boolean, :default => :false
+    add_column :users, :voice_notifications, :boolean, :default => 0
   end
 
   def self.down
