58998b1d5ec2eb89dd7cefa0cd3930045f64f84e
Julien Pervillé <julien.perville@mingalar.fr>
2012-06-30 17:42:44 +0630

    Moved email address configurations to .env.

--- config/initializers/emails.rb
+++ config/initializers/emails.rb
@@ -1,12 +1,14 @@
-## Please uncomment the 4 line below and provide your own set of email adresses.
+## Please define at least DEFAULT_EMAIL (or better: *_EMAIL variables) in .env
+## or modify the lines below at your own risk.
+## To send email you should also check your SMTP_* variables.
 
-Ketchup::Application.config.admin_email = 'Admin <ketchup+admin@localhost>'
-Ketchup::Application.config.contact_email = 'Contact <ketchup+contact@localhost>'
-Ketchup::Application.config.support_email = 'Contact <ketchup+support@localhost>'
-Ketchup::Application.config.notification_email = 'Notification <ketchup+do-not-reply@localhost>'
+Ketchup::Application.config.admin_email = ENV['ADMIN_EMAIL'] || ENV['DEFAULT_EMAIL']
+Ketchup::Application.config.contact_email = ENV['CONTACT_EMAIL'] || ENV['DEFAULT_EMAIL']
+Ketchup::Application.config.support_email = ENV['SUPPORT_EMAIL'] || ENV['DEFAULT_EMAIL']
+Ketchup::Application.config.notification_email = ENV['NOTIFICATION_EMAIL'] || ENV['DEFAULT_EMAIL']
 
 [:admin_email, :contact_email, :support_email, :notification_email].each do |email|
   unless Rails.configuration.respond_to?(email) && Rails.configuration.send(email).present?
-    raise "please provide config.#{email} in #{__FILE__}"
+    raise "please setup DEFAULT_EMAIL or *_EMAIL variables in #{Rails.root}/.env"
   end
 end
