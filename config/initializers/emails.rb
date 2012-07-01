47385ef6a1c327dfdeac2ce1cebf3d7b4686632f
Julien Pervillé <julien.perville@mingalar.fr>
2012-07-01 08:58:41 +0630

    Set a placeholder email address so that console runs without loading `.env`.

--- config/initializers/emails.rb
+++ config/initializers/emails.rb
@@ -2,10 +2,11 @@
 ## or modify the lines below at your own risk.
 ## To send email you should also check your SMTP_* variables.
 
-Ketchup::Application.config.admin_email = ENV['ADMIN_EMAIL'] || ENV['DEFAULT_EMAIL']
-Ketchup::Application.config.contact_email = ENV['CONTACT_EMAIL'] || ENV['DEFAULT_EMAIL']
-Ketchup::Application.config.support_email = ENV['SUPPORT_EMAIL'] || ENV['DEFAULT_EMAIL']
-Ketchup::Application.config.notification_email = ENV['NOTIFICATION_EMAIL'] || ENV['DEFAULT_EMAIL']
+default_email = ENV['DEFAULT_EMAIL'] || 'ketchup@localhost'
+Ketchup::Application.config.admin_email = ENV['ADMIN_EMAIL'] || default_email
+Ketchup::Application.config.contact_email = ENV['CONTACT_EMAIL'] || default_email
+Ketchup::Application.config.support_email = ENV['SUPPORT_EMAIL'] || default_email
+Ketchup::Application.config.notification_email = ENV['NOTIFICATION_EMAIL'] || default_email
 
 [:admin_email, :contact_email, :support_email, :notification_email].each do |email|
   unless Rails.configuration.respond_to?(email) && Rails.configuration.send(email).present?
